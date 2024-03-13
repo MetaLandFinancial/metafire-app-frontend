f46a0c6f01a4b377062f774a79a25914f04234b0
slowpon <slowpon@outlook.com>
2024-03-13 03:49:54 -0400

    add index

---
+++ src/pages/Bnpl/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Hero from "./Hero";
+import Financing from "./Financing";
+import Works from "./Works";
+import Faq from "./Faq";
+import Collaction from "./Collaction";
+
+const Index = () => {
+  return (
+    <div>
+      <Hero />
+      <Collaction />
+      <div className="relative before:content-[''] before:bg-[url('/img/financeWork.png')] before:absolute before:z-0 before:top-0 before:bottom-0 before:left-0 before:right-0 before:bg-no-repeat before:bg-cover full w-full">
+        <Financing />
+        <Works />
+      </div>
+      <Faq />
+    </div>
+  );
+};
+
+export default Index;
