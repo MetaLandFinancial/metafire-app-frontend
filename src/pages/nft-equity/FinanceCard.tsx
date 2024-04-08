7562f0a9f7a8ab601af5874ea4fa27cf4f81b0d6
slowpon <slowpon@outlook.com>
2024-04-07 22:16:09 -0400

    fix error

--- src/pages/nft-equity/FinanceCard.tsx
+++ src/pages/nft-equity/FinanceCard.tsx
@@ -23,7 +23,7 @@ const FinanceCard = ({ nftData }: { nftData: any }) => {
   return (
     <>
       <div className="grid gap-[19px] md:gap-6 grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
-        {nftData.map(({item}:{item:any}, {index}: {index:number}) => (
+        {Array.isArray(nftData) && nftData.map((item, index) => (
           <div className="Finance_Card_BG" key={index}>
             <div className="w-full flex flex-col p-[10px] md:p-5">
               <div  style={{ width: '247px', height: '234px', overflow: 'hidden' }} className="h-full w-full xl:max-w-[247px] rounded Finance_img_shadow overflow-hidden">
