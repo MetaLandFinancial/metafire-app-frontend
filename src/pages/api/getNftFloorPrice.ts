055f4c03d02b7f4309c3d4c207c25629c42f31b3
slowpon <slowpon@outlook.com>
2024-04-10 03:19:57 -0400

    disable proxy in prod

--- src/pages/api/getNftFloorPrice.ts
+++ src/pages/api/getNftFloorPrice.ts
@@ -1,10 +1,9 @@
 // pages/api/opensea/stats.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
 import {HttpsProxyAgent} from 'https-proxy-agent';
-// const HttpsProxyAgent = require('https-proxy-agent');
 import fetch from 'node-fetch';
 const axios = require('axios');
-var request = require('request')
+// var request = require('request')
 
 export default async function handler(
   req: NextApiRequest,
@@ -12,45 +11,28 @@ export default async function handler(
 ) {
   const { collectionSlug } = req.query;
   console.log('collectionSlug', collectionSlug);
+  const PROXY_URL = process.env.PROXY_URL||'';
 
 
     try {
-      const proxyAgent = new HttpsProxyAgent('http://127.0.0.1:7890');
-      // const response = await fetch(`https://api.opensea.io/api/v2/collections/boredapeyachtclub/stats`, {
-      //   method: 'GET',
-      //   headers: {
-      //     'Accept': 'application/json',
-      //     'X-API-KEY': '94e2fcc99ba841e8abaf09eed154e8ed' // Be cautious with your API keys
-      //   },
-      //   agent: 'http://127.0.0.1:7890'
-
-      // });
-
-      const response = await axios.get(`https://api.opensea.io/api/v2/collections/${collectionSlug}/stats`, {
-        httpsAgent: proxyAgent,
+      const requestOptions: any = {
         headers: {
           'Accept': 'application/json',
           'X-API-KEY': '94e2fcc99ba841e8abaf09eed154e8ed',
         },
-      });
+      };
+  
+      // If PROXY_URL is provided, use it as the httpsAgent
+      if (PROXY_URL) {
+        const proxyAgent = new HttpsProxyAgent(PROXY_URL);
+        requestOptions.httpsAgent = proxyAgent;
+      }
+  
+      const response = await axios.get(`https://api.opensea.io/api/v2/collections/${collectionSlug}/stats`, requestOptions);
+  
   
       // Log or process the response data
       console.log('NFT stats', response.status);
-      
-      // let nft;
-      // request({
-      //   url: `https://api.opensea.io/api/v2/collections/boredapeyachtclub/stats`,
-      //   headers: {
-      //     'Accept': 'application/json',
-      //     'X-API-KEY': '94e2fcc99ba841e8abaf09eed154e8ed' 
-      //   },
-      //   host: '127.0.0.1', //我电脑上启动的ss代理地址
-      //   port: 7890 //代理端口
-      // }, function(err:any, resp:any, body:any) {
-      //   // console.log(body)
-      //   nft = body;
-      // })
-      // console.log('response', nft);
 
       if (response.status === 200) {
         const data = await response.data;
