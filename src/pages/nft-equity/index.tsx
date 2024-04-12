46225acb1d891200eff3b541d010fa4d9e005a13
slowpon <slowpon@outlook.com>
2024-04-12 00:13:32 -0400

    fix whitelisted nft showing issue

--- src/pages/nft-equity/index.tsx
+++ src/pages/nft-equity/index.tsx
@@ -13,7 +13,8 @@ type CollectionSlugsType = {
 const collectionSlugs: CollectionSlugsType = {
     "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d" : "boredapeyachtclub",
     "0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258" : "otherdeed",
-    "0x306b1ea3ecdf94aB739F1910bbda052Ed4A9f949" : "beanzofficial"
+    "0x306b1ea3ecdf94aB739F1910bbda052Ed4A9f949" : "beanzofficial",
+    "0xb4ad36f95e84d7b8c1dca17526b37958798ae1ad": "Motherdeed",
   };
 
 const index = () => {
@@ -43,11 +44,11 @@ const index = () => {
         if(data && data.result) {
             // Store the fetched data in state
             // token_address: data.result[0].token_address,
+            console.log(data.result);
             const filteredData = data.result.filter(( item : any ) => {
                 console.log(item.token_address.toLowerCase());
-                Object.keys(collectionSlugs).includes(item.token_address.toLowerCase())
-            }
-            );
+                return Object.keys(collectionSlugs).includes(item.token_address.toLowerCase());
+            });
             console.log(filteredData);
             setTest(data.result[0].name);
             setNftData(filteredData);
