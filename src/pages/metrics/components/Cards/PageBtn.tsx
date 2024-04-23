a2c071a4aaa672df4d256df863d3c25016389b20
Vidhi ZIndex <vidhi.zindex@gmail.com>
2024-04-23 15:05:46 +0530

    added metrics page

--- src/pages/metrics/components/Cards/PageBtn.tsx
+++ src/pages/metrics/components/Cards/PageBtn.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import leftlong from "@/../../public/img/leftlong.svg";
-import leftlongcolored from "@/../../public/img/leftlongcolored.svg";
+import React from 'react'
+import leftlong from "../../../../../public/img/leftlong.svg";
+import leftlongcolored from "../../../../../public/img/leftlongcolored.svg";
 import Image from "next/image";
-const PaginationBtn = () => {
-  const num = [{ nm: 1 }, { nm: 2 }, { nm: 3 }, { nm: 4 }];
+const PageBtn = () => {
+    const num = [{ nm: 1 }, { nm: 2 }, { nm: 3 }, { nm: 4 }];
   return (
-    <>
-      <div className="hidden md:flex relative mt-16 md:mt-20 w-fit flex-row gap-[10px] mx-auto">
+    <div>
+       <div className="hidden lg:flex relative lg:mt-12 w-fit flex-row gap-[10px] mx-auto">
         <div className="Pagination_Btn flex justify-center items-center border-[1px] border-[#798295] rounded-[7px] h-10 w-10 md:h-12 md:w-12 text-white  hover:border-[#7982955] duration-500 opacity-[0.50] hover:opacity-[1] cursor-pointer">
           <Image
             src={leftlong}
@@ -48,8 +48,8 @@ const PaginationBtn = () => {
           />
         </div>
       </div>
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default PaginationBtn;
+export default PageBtn
---
+++ src/pages/metrics/components/main-page/StakeBtnMetrics.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import Image from "next/image";
+import metricsC1 from "@/../../public/img/metricsC1.svg";
+import metricsC2 from "@/../../public/img/metricsC2.svg";
+import metricsC3 from "@/../../public/img/metricsC3.svg";
+import metricsC4 from "@/../../public/img/metricsC4.svg";
+import Slider from "react-slick";
+const StakeBtnMetrics = () => {
+  const metricsData = [
+    {
+      image: metricsC1,
+      label: "MetaFire",
+    },
+    {
+      image: metricsC2,
+      label: "ETH",
+    },
+    {
+      image: metricsC3,
+      label: "MATIC",
+    },
+    {
+      image: metricsC4,
+      label: "Optimism",
+    },
+  ];
+  const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    swipeToSlide: true,
+    slidesToShow: 4,
+    speed: 2000,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 680,
+        settings: {
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 420,
+        settings: {
+          slidesToShow: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 350,
+        settings: {
+          slidesToShow: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="relative block overflow-hidden lg:hidden">
+      <div className="Metrics_Slider ml-6">
+        <div className="slider-container">
+          <Slider {...settings}>
+            {metricsData.map((metric, index) => (
+              <div
+                key={index}
+                className="w-fit rounded-xl border border-[#4776E6] slide_M hover:border-transparent hover:bg-gradient-to-r hover:from-[#4776E6] hover:to-[#8E54E9]"
+              >
+                <Image
+                  src={metric.image}
+                  alt="metrics"
+                  height={20}
+                  width={20}
+                  className="h-5 w-5 rounded-full"
+                />
+                <p className="text-sm font-semibold leading-[150%] tracking-[0.15px] text-white">
+                  {metric.label}
+                </p>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StakeBtnMetrics;
