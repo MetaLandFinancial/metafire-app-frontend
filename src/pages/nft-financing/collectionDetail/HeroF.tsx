ed2b12c59ef7bffaf6c01e72b0ef729cdf566e97
slowpon <slowpon@outlook.com>
2024-04-29 04:54:43 -0400

    update

--- src/pages/nft-financing/collectionDetail/HeroF.tsx
+++ src/pages/nft-financing/collectionDetail/HeroF.tsx
@@ -8,13 +8,13 @@ const HeroF = () => {
       <div className="container">
         <div className="pt-[55px] pb-[17px] md:pt-[58px] md:pb-[34px]">
           <div className="rounded-full h-[61px] w-[61px] md:h-[149px] md:w-[149px] relative z-10">
-            <Image
+            {/* <Image
               src="/img/monkey.png"
               alt="bgImg"
               className="w-full h-full"
               width={61}
               height={61}
-            />
+            /> */}
           </div>
         </div>
       </div>
