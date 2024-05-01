9d14cfde2dcb4fc3b34af83099b8994212b7a2f2
slowpon <slowpon@outlook.com>
2024-05-01 04:43:37 -0400

    add text

--- src/pages/nft-equity/FinanceMain.tsx
+++ src/pages/nft-equity/FinanceMain.tsx
@@ -14,6 +14,9 @@ const FinanceMain = ({ nftData, signer}: { nftData: any, signer: any }) => {
           {/* <div style={{color:"white"}} >sadas2 {nftData?.[0]?.name} </div> */}
           <div className="w-full">
             {/* <FinInput /> */}
+            <p className="text-center text-white font-bold text-lg md:text-xl mb-5">
+              Only the NFTs you own that are eligible for loans will show up below: Please see our Whitelist Link
+            </p>
             <FinanceCard nftData={nftData} signer={signer} />
           </div>
         </div>
