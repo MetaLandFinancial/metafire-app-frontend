5a47751d37a960564128359e2a9e28b13669d267
slowpon <slowpon@outlook.com>
2024-05-17 06:16:32 -0400

    add docs

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -184,7 +184,7 @@ export const footerOne: footerOne[] = [
 
     {
         name: "How it Works",
-        link: "#",
+        link: "https://app.gitbook.com/o/1eo3vi9ZZq99yiOBWxuw/s/4mbNYRKmLy3W4JoSV2Az/",
     },
 
     {
